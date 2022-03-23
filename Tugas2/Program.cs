033e8859541f766a01ac64d7e7a1223c6175808f
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-03-23 22:51:53 +0700

    Add project files.

---
+++ Tugas2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tugas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public void InsertTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-OG8P1VHL\\HIJJAJ;database=sewatoko;Integrated Security = TRUE");
+                con.Open();
+            } catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
